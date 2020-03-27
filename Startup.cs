2421fa53e62a8c73be38b63c635827740121a8f3
30james00 <30james00@users.noreply.github.com>
2020-03-27 16:54:11 +0100

    ADD basic swagger DELETE junk

--- Startup.cs
+++ Startup.cs
@@ -1,9 +1,13 @@
+using System;
+using System.Reflection;
+using System.IO;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 using CvApi.Models;
 
 namespace CvApi
@@ -22,10 +26,21 @@ namespace CvApi
             services.AddDbContext<ContactContext>(opt =>
                opt.UseSqlite(Configuration.GetConnectionString("DefaultConnection")));
             services.AddControllers();
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Contact", Version = "v1" });
+            });
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Contact");
+            });
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
